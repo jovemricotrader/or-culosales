b426f875e88ce15fcd77acde2ed18c2aee61171b
jovemricotrader <canalsemcueca@gmail.com>
2026-03-30 19:41:29 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,21 +1,11 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-
-const PORT = process.env.PORT || 3000;
-
-http.createServer((req, res) => {
-  // Security headers
-  res.setHeader('X-Content-Type-Options', 'nosniff');
-  res.setHeader('X-Frame-Options', 'SAMEORIGIN');
-  res.setHeader('X-XSS-Protection', '1; mode=block');
-  
-  const file = path.join(__dirname, 'fim-das-opcoes.html');
-  fs.readFile(file, (err, data) => {
-    if (err) { res.writeHead(404); res.end('Not found'); return; }
-    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+const http=require('http'),fs=require('fs'),path=require('path');
+const PORT=process.env.PORT||3000;
+http.createServer((req,res)=>{
+  res.setHeader('X-Frame-Options','SAMEORIGIN');
+  res.setHeader('X-Content-Type-Options','nosniff');
+  fs.readFile(path.join(__dirname,'fim-das-opcoes.html'),(err,data)=>{
+    if(err){res.writeHead(404);res.end('Not found');return;}
+    res.writeHead(200,{'Content-Type':'text/html; charset=utf-8'});
     res.end(data);
   });
-}).listen(PORT, () => {
-  console.log('JR ORACULO SITE porta ' + PORT);
-});
+}).listen(PORT,()=>console.log('JR ORACULO SITE :'+PORT));
